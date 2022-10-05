e29d71519253552d6c8301ccab1e7dac6f07924e
Lincoln Konig <lincolnkonig5@gmail.com>
2022-10-04 23:09:33 -0300

    initial commit

---
+++ Kicker/BoundingSphere.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef BOUNDING_SPHERE_INCLUDED_H
+#define BOUNDING_SPHERE_INCLUDED_H
+
+class BoundingSphere
+{
+public:
+private:
+
+};
+
+#endif // !BOUNDING_SPHERE_INCLUDED_H
+
+
+
